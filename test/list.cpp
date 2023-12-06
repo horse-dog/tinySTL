b1252c2019ded41c69f99003690cbae1188d3a52
horse-dog <1418411576@qq.com>
2023-12-06 21:49:07 +0800

    cm

--- test/list.cpp
+++ test/list.cpp
@@ -113,3 +113,112 @@ TEST(list, assign) {
     EXPECT_EQ(li.size(), 3);
   }
 }
+
+TEST(list, assign_operator) {
+  /**
+   * @test list& operator=(initializer_list l)
+   * @brief assign operator by initializer_list.
+   */
+  SUBTEST(operator=) {
+    list<int> li = {1, 2, 3, 4};
+    li = {1, 2, 3};
+    EXPECT_STRING_EQ(li, [1, 2, 3]);
+    EXPECT_EQ(li.size(), 3);
+  }
+  
+  /**
+   * @test list& operator=(const list& x)
+   * @brief assign operator by copy.
+   */
+  SUBTEST(operator=) {
+    list<int> li0 = {1, 2, 3, 4};
+    list<int> li1 = {1, 2, 3};
+    li0 = li1;
+    EXPECT_STRING_EQ(li0, [1, 2, 3]);
+    EXPECT_EQ(li0.size(), 3);
+  }
+
+  /**
+   * @test list& operator=(list&& x)
+   * @brief assign operator by move.
+   */
+  SUBTEST(operator=) {
+    list<int> li = {1, 2, 3, 4};
+    li = list<int>({1, 2, 3});
+    EXPECT_STRING_EQ(li, [1, 2, 3]);
+    EXPECT_EQ(li.size(), 3);
+  }
+}
+
+TEST(list, back) {
+  list<int> li;
+  EXPECT_THROW(li.back(), std::range_error);
+  li = {1, 2, 3};
+  EXPECT_EQ(li.back(), 3);
+  li.back() = 33;
+  EXPECT_EQ(li.back(), 33);
+}
+
+TEST(list, begin) {
+  list<int> li = {1, 2, 3};
+  EXPECT_EQ(*li.begin(), 1);
+  *li.begin() = 11;
+  EXPECT_EQ(*li.begin(), 11);
+}
+
+TEST(list, clear) {
+  list<int> li = {1, 2, 3};
+  li.clear();
+  li.clear();
+  EXPECT_EQ(li.size(), 0);
+}
+
+TEST(list, emplace) {
+  list<TestClass> li;
+  li.emplace(li.begin(), 2, 3);
+  EXPECT_STRING_EQ(li, [[3, 3]]);
+  li.emplace(li.begin(), 3, 4);
+  EXPECT_STRING_EQ(li, [[4, 4, 4], [3, 3]]);
+}
+
+TEST(list, emplace_back) {
+  list<int> li = {1, 2, 3};
+  li.emplace_back(4);
+  EXPECT_STRING_EQ(li, [1, 2, 3, 4]);
+}
+
+TEST(list, empty) {
+  list<int> li = {1, 2, 3};
+  EXPECT_EQ(li.empty(), false);
+  li.clear();
+  EXPECT_EQ(li.empty(), true);
+}
+
+TEST(list, end) {
+  list<int> li = {1, 2, 3};
+  auto it = li.end();
+  EXPECT_EQ(*--it, 3);
+}
+
+TEST(list, erase) {
+  /**
+   * @test  iterator erase(const_iterator pos)
+   * @brief erase an item.
+   */
+  SUBTEST(erase) {
+    list<int> li = {1, 2, 3};
+    auto it = li.erase(li.begin());
+    EXPECT_STRING_EQ(li, [2, 3]);
+    EXPECT_EQ(*it, 2);
+  }
+
+  /**
+   * @test  iterator erase(const_iterator first, const_iterator last)
+   * @brief range erase.
+   */
+  SUBTEST(erase) {
+    list<int> li = {1, 2, 3};
+    li.erase(++li.begin(), li.end());
+    EXPECT_STRING_EQ(li, [1]);
+  }
+}
